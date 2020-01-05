6fe50331b365ae838413e25004f7785512830c2f
Terodedi <terodedi1999@gmail.com>
2020-01-05 21:03:07 +0100

    Tabla Grupos Turistas

---
+++ ProyectoRutasVino/src/Presentacion/TablaGruposTuristas.java
@@ -0,0 +1,50 @@
+package Presentacion;
+
+import java.util.*;
+import javax.swing.table.*;
+
+class TablaGruposTuristas extends AbstractTableModel {
+	private String[] nombreColumnas = { "Circuito", "Nº personas", "Guía/Idioma", "Coste (€)", "Realizado" };
+	private Vector<Object[]> datos = new Vector<Object[]>();
+
+	public int getColumnCount() {
+		return nombreColumnas.length;
+	}
+
+	public int getRowCount() {
+		return datos.size();
+	}
+
+	public String getColumnName(int col) {
+		return nombreColumnas[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		Object[] fila = (Object[]) datos.elementAt(row);
+		return fila[col];
+	}
+
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	public boolean isCellEditable(int row, int col) {
+		return true;
+	}
+
+	public void setValueAt(Object value, int row, int col) {
+		if (row < getRowCount() && col < getColumnCount()) {
+			Object[] fila = (Object[]) datos.elementAt(row);
+			fila[col] = value;
+			fireTableCellUpdated(row, col);
+		}
+	}
+
+	public void eliminaFila(int row) {
+		datos.remove(row);
+	}
+
+	public void anadirFila(Object[] row) {
+		datos.add(row);
+	}
+}
